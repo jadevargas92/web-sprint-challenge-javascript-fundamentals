2bd67196c52626368a2b9d51e05174bba49b760a
Jade <jadevargas92@gmail.com>
2021-02-21 11:34:03 -0600

    Testing Code Grade Work Flow

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Testing Code Grade Work flow
+
 //🚀🚀🚀  Topic #1 Closures 🚀🚀🚀//
 /* 🚀🚀🚀🤓 Task 1: 🤓🚀🚀🚀 
 Study the code below and explain in your own words why nested function can access the variable internal. */
